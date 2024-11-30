e46fb6daecc19d2244bc5aab8ae27692b71af9d3
ErnestoTSantos <ernesto.terra2003@gmail.com>
2024-11-30 09:59:08 -0300

    feat: configure models, admin and jwt login

---
+++ yellot_mob/modules/chat/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+
+from yellot_mob.modules.chat.models import ChatRoom
+from yellot_mob.modules.chat.models import ChatMessage
+
+
+@admin.register(ChatRoom)
+class ChatRoomAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "created_at", "updated_at")
+    list_filter = ("name",)
+
+
+@admin.register(ChatMessage)
+class ChatMessageAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "room", "created_at", "updated_at")
+    list_filter = ("user", "room__name",)
---
+++ yellot_mob/modules/chat/models.py
@@ -0,0 +1,26 @@
+import uuid
+
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class ChatRoom(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class ChatMessage(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    room = models.ForeignKey(ChatRoom, on_delete=models.CASCADE)
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.user} - {self.message}"
